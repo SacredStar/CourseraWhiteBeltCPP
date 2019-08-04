8664a440591448a680ba2d0b15f72464037dd542
shmelevka <shadowordeath1993@yandex.ru>
2019-08-04 22:39:18 +0300

    Completed task

---
+++ Автобусные остановки/Автобусные остановки/Source.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <vector>
+
+
+using namespace std;
+void NEW_BUS_TEST(string bus_name, int stop_count,  multimap <string, string>& bus, vector <string>& bus_stops)
+{
+	string stop_name;
+	while (stop_count != 0)
+	{		
+		cin >> stop_name;
+	}
+}
+
+void BUSES_FOR_STOP() 
+{
+
+}
+void STOPS_FOR_BUS()
+{
+
+}
+
+void ALL_BUSES()
+{
+
+}
+
+void main()
+{
+	multimap <string, string> bus;
+	//map <string, string> bus;
+	vector <string> bus_stops;
+
+}
